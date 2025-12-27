55c2290fb952777c39df8cd980ccd6cefec5a52f
Mason Anton <masonanton1@gmail.com>
2025-12-27 15:27:48 -0500

    Prints added for button, axis, and hats motion.
    Added prints for debugging to the main loop. Added handling for quitting
    which will be used later when the screen is actually rendered.

--- src/main.py
+++ src/main.py
@@ -7,13 +7,41 @@ try:
 except:
     print("Error in intialization.")
 
-while (pygame.joystick.get_count() == 0):
+while pygame.joystick.get_count() == 0:
     pygame.event.pump()
     print("No joystick detected.")
     sleep(1)
 
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
+
+print("hats", joystick.get_numhats())
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    if event.type == pygame.JOYAXISMOTION:
+        print(
+            "Axis 0:", joystick.get_axis(0),
+            "Axis 1:", joystick.get_axis(1),
+            "Axis 2:", joystick.get_axis(2),
+            "Axis 3:", joystick.get_axis(3)
+        )
+
+    if event.type == pygame.JOYBUTTONDOWN:
+        print(
+            event.dict
+        )
+
+    if event.type == pygame.JOYHATMOTION:
+        print(
+            "Hat 0:", joystick.get_hat(0)
+        )
+
+
 
 
 
